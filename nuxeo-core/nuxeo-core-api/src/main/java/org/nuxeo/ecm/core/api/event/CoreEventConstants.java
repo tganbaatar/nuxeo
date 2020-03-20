5a1705c76f10d8452f790d315039ecc0bba8c75f
Salem Aouana <saouana@nuxeo.com>
2020-03-20 17:47:21 +0100

    NXP-28784: Record management - Missing labels in the audit for retention events

--- nuxeo-core/nuxeo-core-api/src/main/java/org/nuxeo/ecm/core/api/event/CoreEventConstants.java
+++ nuxeo-core/nuxeo-core-api/src/main/java/org/nuxeo/ecm/core/api/event/CoreEventConstants.java
@@ -127,16 +127,19 @@ public final class CoreEventConstants {
     public static final String RESET_CREATOR = "resetCreator";
 
     /**
-     * Passed with {@value DocumentEventTypes#BEFORE_SET_RETENTION} and {@value DocumentEventTypes#AFTER_SET_RETENTION}
-     * events, the retention datetime (a {@link Calendar} object).
+     * Passed with {@value DocumentEventTypes#BEFORE_SET_RETENTION},{@value DocumentEventTypes#BEFORE_EXTEND_RETENTION},
+     * {@value DocumentEventTypes#AFTER_SET_RETENTION} and {@value DocumentEventTypes#AFTER_EXTEND_RETENTION} events,
+     * the retention datetime (a {@link java.util.Calendar} object).
      *
      * @since 11.1
      */
     public static final String RETAIN_UNTIL = "retainUntil";
 
     /**
-     * Passed with {@value DocumentEventTypes#BEFORE_SET_LEGAL_HOLD} and
-     * {@value DocumentEventTypes#AFTER_SET_LEGAL_HOLD} events, the legal hold status (a {@link Boolean} object).
+     * Passed with
+     * {@value DocumentEventTypes#BEFORE_SET_LEGAL_HOLD},{@value DocumentEventTypes#BEFORE_REMOVE_LEGAL_HOLD},
+     * {@value DocumentEventTypes#AFTER_SET_LEGAL_HOLD} and {@value DocumentEventTypes#AFTER_REMOVE_LEGAL_HOLD} events,
+     * the legal hold status (a {@link Boolean} object).
      *
      * @since 11.1
      */
